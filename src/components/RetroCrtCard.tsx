07874de944966e3dee87cc3d6d6b965815bee27f
scottino2019-glitch <scottino2019@gmail.com>
2026-07-23 11:17:39 +0200

    Add files via upload

--- src/components/RetroCrtCard.tsx
+++ src/components/RetroCrtCard.tsx
@@ -20,7 +20,7 @@ const RetroCrtCard = () => {
           </a>
 
           <p className="output-line success">
-            &gt; Imparare Html e Css:{" "}
+            &gt; Impare Html e Css:{" "}
             <a
               href="https://impara-html-e-css-579414217719.europe-west2.run.app/"
               target="_blank"
@@ -52,9 +52,9 @@ const RetroCrtCard = () => {
             </a>
           </p>
 
-          <br />
+        <br />
 
-          <a
+       <a
             href="https://disegno-vettoriale.vercel.app/"
             className="output-line"
             target="_blank"
@@ -62,8 +62,7 @@ const RetroCrtCard = () => {
           >
             &gt; Disegno Vettoriale
           </a>
-
-          <p className="output-line success">
+           <p className="output-line success">
             &gt; Lettore JSON Universale:{" "}
             <a
               href="https://lettore-json-universale.vercel.app/"
@@ -73,6 +72,7 @@ const RetroCrtCard = () => {
               OK
             </a>
           </p>
+          
         </div>
 
         <div className="screen-loader">
